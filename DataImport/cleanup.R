f43819a8a43bac03e0ae6ab68c2854e021a576a4
Jordan Poles <jpoles1@gmail.com>
2016-05-25 23:01:37 -0400

    cleaner variable names

--- DataImport/cleanup.R
+++ DataImport/cleanup.R
@@ -3,17 +3,17 @@ clean_data = function(df){
   remove_cols = c("OBJECTID", "OBJECTID_1", "STATE", "Total")
   return(df[!(names(df) %in% remove_cols)])
 }
-raw_censusData = clean_data(read.csv("DataImport/Census_2010_Tracts.csv"))
-raw_householdIncome = clean_data(read.csv("DataImport/Median_Household_Income_by_Census_Block_Group_2010.csv"))
-raw_homeValue = clean_data(read.csv("DataImport/Median_House_Value_by_Census_Block_Group_2010.csv"))
-raw_langData = clean_data(read.csv("DataImport/Language_Spoken_at_Home_by_Census_Block_Group_2010.csv"))
-raw_edData = clean_data(read.csv("DataImport/Educational_Attainment_by_Census_Block_Group_2010.csv"))
+censusData = clean_data(read.csv("DataImport/Census_2010_Tracts.csv"))
+householdIncome = clean_data(read.csv("DataImport/Median_Household_Income_by_Census_Block_Group_2010.csv"))
+homeValue = clean_data(read.csv("DataImport/Median_House_Value_by_Census_Block_Group_2010.csv"))
+langData = clean_data(read.csv("DataImport/Language_Spoken_at_Home_by_Census_Block_Group_2010.csv"))
+edData = clean_data(read.csv("DataImport/Educational_Attainment_by_Census_Block_Group_2010.csv"))
 #Merge where possible
 merge_frames = function(x, y){
   merge(x, y, by=intersect(names(x), names(y)), all=1)
 }
 
-clean_data = merge_frames(raw_householdIncome, raw_homeValue)
-clean_data = merge_frames(clean_data, raw_langData)
-clean_data = merge_frames(clean_data, raw_edData)
+clean_data = merge_frames(householdIncome, homeValue)
+clean_data = merge_frames(clean_data, langData)
+clean_data = merge_frames(clean_data, edData)
 write.csv(clean_data, "clean_data.csv")
