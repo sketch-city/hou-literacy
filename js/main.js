b980a8d91a1fde59fd4a95ba4f50987925b40edf
Adrian Gonzales <adrian@uptrending.com>
2016-06-20 12:04:08 -0500

    Basic Vue work done. Needs hover view/interaction

--- js/main.js
+++ js/main.js
@@ -1,69 +1,204 @@
-String.prototype.capitalizeFirstLetter = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+
+// Map of terms to use in the UI and their labels
+var litmap_term_map = {
+  No_HS : "No High School",
+  Percent_No_HS : "% No High School",
+  Percent_Only_Spanish : "% Only Spanish",
+  Percent_Other_Languages : "% Other Languages",
+  HS_Graduate : "High School Graduate",
+  Some_College : "Some College",
+  BS_and_Higher : "BS and Higher",
+  Percent_HS_Graduate : "% High School Graduate",
+  Percent_Some_College : "% Some College",
+  Percent_BS_and_Higher : "% BS and Higher",
+  Median_HHI : "Median Household Income",
+  Median_House_Value : "Median House Value",
+  Only_English : "Only English",
+  Only_Spanish : "Only Spanish",
+  Other_Languages : "Other Languages",
+  Percent_Only_English : "% Only English",
 }
 
-var color_scale;
-
-// Default Property to highlight
-var current_prop = "No_HS"
-
-// Begin Application
-$(function(){
-
-  //Setup leaflet map
-  var houstonmap = L.map('houstonmap').setView([29.7604, -95.3698], 12);
-
-  // Grab tile layer
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> with <a href="https://github.com/SINTEF-9012/PruneCluster" target="_blank">PruneCluser</a>. More info about this <a href="http://data.houstontx.gov/dataset/city-of-houston-parking-citations">data</a> can be found in my <a href="https://github.com/jpoles1/HOUTix/blob/master/README.md">report (source, methodology)</a>'
-  }).addTo( houstonmap ); // Add to map
 
 
-  // Grab JSON literacy data from server
-  $.getJSON("literacy_data.json", function(lit_data){
 
-    // Prepare interface
-    function prepareInfoarea( block_id ){
+////////////////////////////////
+////////////////////////////////
+// Main Litmap Application
+var litmap = new Vue({
+  el: '.litmap', // Target Element for App
 
-      // Clear the interface
-      $("#infoarea").html("");
+  data: { // Data we're binding to
+    literacy_data: null,
+    color_scale : null, // Color Scale for Heatmap
+    boundaries : null,
+    map_view: null,
+    term_map: litmap_term_map,
+    current_prop: "No_HS", // Property to highlight
+    block_id: null, // Property to highlight
+    current_block: null, // Data for currently highlighted block
 
-      // Loop through properties, adding buttons for each
-      for(prop in lit_data){
+    map : {
+        element : "houstonmap",
+        center : [29.7604, -95.3698],
+        zoom: 12,
+        style: { //    http://leafletjs.com/reference.html#geojson-style
+            color : "red",
+            fillOpacity: .7,
+            fillColor: "white",
+            weight: 1,
+            pointerEvents: "visiblePainted"
+        }
+    },
 
-        // Check for active property
-        var active = (prop == current_prop ? "active": "");
-
-        // Append button
-        $("#infoarea").append("<button class='prop_button "+active+"' type='button' value="+prop+">"+prop+(block_id ? ": "+lit_data[prop][block_id] : "")+"</button>")
-      }
+    datasources : {
+      literacy_data : "literacy_data.json",
+      census_blocks : "census_blocks.kml",
+      literacy_providers : "lit_providers.kml",
+      tile_layer : "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
     }
+  },
 
-    // Immediately create interface
-    prepareInfoarea();
+  // Initialize application
+  created: function () {
+    var self = this;
 
-    //Setup geo-data
-    function highlightFeature( e ) {
+    // Fetch the data
+    this.fetchData(function( response ){
+      // Update the literacy data
+      self.literacy_data = response.data;
+
+      // Callback on successful fetch
+      self.createMap();
+    });
+  },
+
+  // Watch for changes to these properties
+  watch: {
+    current_prop: 'changeFeature', // Fire changeFeature when current_prop changes
+    block_id: 'blockData' // Fire blockData when block_id changes
+  },
+
+  // Methods used in the application
+  methods: {
+
+    // Fetch data from the server
+    fetchData: function ( success_callback, error_callback ) {
+      var self = this;
+
+      // GET request
+      this.$http( { url: self.datasources.literacy_data , method: 'GET' } )
+        .then( function( response ) {
+          // success callback
+          console.log( "SUCCESS!", response );
+
+          if( typeof success_callback == "function" ){
+              success_callback( response );
+          }
+
+        }, function(response) {
+            // error callback
+            console.log( "FAIL!", response );
+
+            if( typeof error_callback == "function" ){
+                error_callback( response );
+            }
+        });
+    },
+
+
+    // Create Map View
+    createMap: function(){
+      var self = this;
+
+      //Setup leaflet map
+      this.map_view = L.map( this.map.element ).setView( this.map.center, this.map.zoom );
+
+      // Grab tile layer
+      L.tileLayer( this.datasources.tile_layer, {
+        attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> with <a href="https://github.com/SINTEF-9012/PruneCluster" target="_blank">PruneCluser</a>. More info about this <a href="http://data.houstontx.gov/dataset/city-of-houston-parking-citations">data</a> can be found in my <a href="https://github.com/jpoles1/HOUTix/blob/master/README.md">report (source, methodology)</a>'
+      }).addTo( this.map_view ); // Add to map
+
+      // Set boundaries for layers w/ geoJSON notation
+      // http://leafletjs.com/reference.html#geojson-style
+      this.boundaries = L.geoJson(null, {
+        style: self.map.style,
+        onEachFeature: self.onEachFeature
+      });
+
+      // Ingest the Cenus Blocks KML data
+      omnivore.kml("census_blocks.kml", null, this.boundaries);
+
+      // Set options for map markers
+      var markerOpts = {
+        radius: 15,
+        fillColor: "blue",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: .8
+      };
+
+      // Set image path
+      L.Icon.Default.imagePath = "img/"
+
+      // Create markers for literacy centers
+      var lit_markers = L.geoJson(null, {
+        pointToLayer: function (feature, latlng) {
+          //console.log( feature )
+          return L.marker(latlng).bindPopup(feature.properties.name);
+        }
+      });
+
+      // Ingest Providers KML and apply markers
+      omnivore.kml("lit_providers.kml", null, lit_markers);
+
+      // Add Providers to the map
+      lit_markers.addTo( this.map_view );
+
+      // add boundaries to the map
+      this.boundaries
+            .addTo( this.map_view )
+            .on('ready', this.changeFeature ); // Fire changeFeature on load
+    },
+
+    // Composite Feature ID from data row index
+    getFeatureID: function(row){
+      return( row["TRACT"] + "-" + row["BLKGRP"] );
+    },
+
+
+    // Function for changing map feature
+    changeFeature: function(){
+      var self = this;
+
+      // Get color scale from
+      this.color_scale = chroma
+                          .scale(['yellow', 'red']) // Color range
+                          .domain([ this.literacy_data[ self.current_prop ]["min"], this.literacy_data[ self.current_prop ]["max"] ]); // Min/max from data
+
+      // Loop through each layer
+      this.boundaries.eachLayer( function( layer ){
+
+        // Get the block_id from the current layer
+        var block_id = self.getFeatureID( layer.feature.properties );
+
+        // Set the sytle based on the calculated scale
+        layer.setStyle({
+          fillColor : self.color_scale( self.literacy_data[ self.current_prop ][ block_id ] ).hex()
+        });
+
+      });
+    }, // END CHANGE FEATURE
+
+    // Highlight Feature
+    highlightFeature: function( e ){
+      console.log( "Mouseover!" );
+
       var layer = e.target; // Select the layer that was clicked
       var layer_props = layer.feature.properties; // Grab Leaflet properties
-      var block_id = getFeatureID( layer_props ); // Find the block_id
 
       // Build Interface with chosen Block ID
-      prepareInfoarea( block_id );
-
-      // Listen to clicks on each property button
-      $(".prop_button").click(function(){
-        // Set the global current property
-        current_prop = $(this).val();
-        console.log(current_prop);
-
-        // Change the feature
-        changeFeature();
-
-        // Switch classes on the buttons
-        $(".prop_button").removeClass("active");
-        $(this).addClass("active");
-      });
+      this.block_id = this.getFeatureID( layer_props );
 
       // Set layer opacity after it's been loaded
       layer.setStyle({
@@ -74,129 +209,65 @@ $(function(){
       if (!L.Browser.ie && !L.Browser.opera) {
           layer.bringToFront();
       }
-    }
+    },
 
     // Reset the highlighted layers
-    function resetHighlight(e) {
+    resetHighlight: function(e) {
       e.target.setStyle({
           fillOpacity: .7
       });
-    }
+    },
 
     // Zoom
-    function zoomToFeature(e) {
+    zoomToFeature: function(e) {
       //Adds a % padding to a frame around the feature of interest
-      houstonmap.fitBounds(e.target.getBounds().pad(.5));
-    }
+      this.map_view.fitBounds( e.target.getBounds().pad( .5 ) );
+    },
 
     // Loop for each feature
-    function onEachFeature(feature, layer) {
+    onEachFeature: function(feature, layer) {
+      var self = this;
+
       layer.on({
-          mouseover: highlightFeature,
-          mouseout: resetHighlight,
-          click: zoomToFeature
+          mouseover: self.highlightFeature,
+          mouseout: self.resetHighlight,
+          click: self.zoomToFeature
       });
-    }
+    },
 
-    // Composite Feature ID from data row index
-    getFeatureID = function(row){
-      return( row["TRACT"] + "-" + row["BLKGRP"] );
-    }
+    // Gather all of the data for a single block
+    blockData: function(){
+      var data = [];
 
-    // Set boundaries for layers w/ geoJSON notation
-    // http://leafletjs.com/reference.html#geojson-style
-    var boundaries = L.geoJson(null, {
-      style: {
-        color : "black",
-        fillOpacity: .7,
-        fillColor: "white"
-      },
-      onEachFeature: onEachFeature
-    });
+      // Make sure we have a block_id
+      if( this.block_id ){
 
-    // Function for changing features
-    // Fired when an Interface Button is clicked
-    var changeFeature = function(){
-      console.log(current_prop);
+        // Loop through our property map
+        for( property in this.term_map ){
 
-      // Get color scale from
-      color_scale = chroma
-                      .scale(['yellow', 'red']) // Color range
-                      .domain([ lit_data[current_prop]["min"], lit_data[current_prop]["max"] ]); // Min/max from data
+          // If we have data for this property and this block
+          if( this.literacy_data[ property ].indexOf( this.block_id ) > -1 ) {
 
-      // Loop through each layer
-      boundaries.eachLayer(function(layer){
+              // Add it to the array
+              data.push( this.literacy_data[ property ][ this.block_id ] );
+          }
+        }
 
-        // Get the block_id from the current layer
-        var block_id = getFeatureID( layer.feature.properties );
-
-        // Set the sytle based on the calculated scale
-        layer.setStyle({
-          fillColor : color_scale( lit_data[current_prop][block_id] ).hex()
-        });
-      });
-    } // END CHANGE FEATURE
-
-
-    // Loop through the data again
-    for(key in lit_data){
-      // Add an option to dropdown selectors
-      $(".variable-selector").append( '<option value="' + key + '">' + key + '</option>' )
-    }
-
-    // Set the selector to the current property
-    $(".variable-selector").val(current_prop)
-
-    // Listen to change actions on the selector
-    $(".variable-selector").on("change", function(){
-      current_prop = $(this).val();
-      console.log(current_prop);
-      changeFeature();
-    })
-
-    // Ingest the Cenus Blocks KML data
-    omnivore.kml("census_blocks.kml", null, boundaries);
-
-    // Set options for map markers
-    var markerOpts = {
-      radius: 15,
-      fillColor: "blue",
-      weight: 1,
-      opacity: 1,
-      fillOpacity: .8
-    };
-
-    // Set image path
-    L.Icon.Default.imagePath = "img/"
-
-    // Create markers for literacy centers
-    var lit_markers = L.geoJson(null, {
-      pointToLayer: function (feature, latlng) {
-        console.log(feature)
-        return L.marker(latlng).bindPopup(feature.properties.name);
+        // If we have data
+        if( data.length ){
+          // Set the model data in Vue
+          this.current_block = data;
+          return;
+        }
       }
-    });
 
-    // Ingest Providers KML and apply markers
-    omnivore.kml("lit_providers.kml", null, lit_markers);
+      // No data
+      this.current_block = null;
 
-    // Add Providers to the map
-    lit_markers.addTo(houstonmap);
+    } // END block_data
 
-    // add boundaries to the map
-    boundaries
-          .addTo(houstonmap)
-          .on('ready', changeFeature ); // Fire changeFeature on load
-  });
 
-  // Handle tab menu clicks
-  $(".tabs-menu a").click(function(event) {
-    event.preventDefault();
-    $(this).parent().addClass("current");
-    $(this).parent().siblings().removeClass("current");
-    var tab = $(this).attr("href");
-    $(".tab-content").not(tab).css("display", "none");
-    $(tab).fadeIn();
-  });
+  }
 
-}); // END PAGE LOAD
+
+});
